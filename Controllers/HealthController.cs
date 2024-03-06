2132cca190f1c0a76516a580915bb04ab07a9370
Pedro Oriani <125466492+PedroOriani@users.noreply.github.com>
2024-03-06 07:18:27 -0300

    Fix: Update Health Controller

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -3,22 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using Pix.Services;
 
 namespace Pix.Controllers;
-
-[ApiController]
-[Route("[controller]")] //define qual a rota
-public class HealthController : ControllerBase
 {
-
-    private HealthService _healthService
-
-    public HealthController(HealthService healthService)
+    [ApiController]
+    [Route("[controller]")] //define qual a rota
+    public class HealthController : ControllerBase
     {
-        _healthService = healthService;
+        private readonly HealthService _healthService
+    
+        public HealthController(HealthService healthService)
+        {
+            _healthService = healthService;
+        }
+    
+        [HttpGet] //define o tipo de requisição
+        public IActionResult GetHealth()
+        {
+            return Ok(_healthService.GetHealthMessage());
+        }
     }
-
-    [HttpGet] //define o tipo de requisição
-    public IActionResult GetHealth()
-    {
-        return Ok(_healthService.GetHealthMessage());
-    }
-} 
+}
