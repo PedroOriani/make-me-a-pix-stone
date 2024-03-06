9b73149a6c7a9329a7c591a081a881517fe3d67d
Pedro Oriani <125466492+PedroOriani@users.noreply.github.com>
2024-03-06 07:20:14 -0300

    Fix: Update HealthService

--- Services/HealthService.cs
+++ Services/HealthService.cs
@@ -1,17 +1,18 @@
 using Pix.Repositories;
 
 namespace Pix.Services;
-
+{
     public class HealthService
     {
-
-        private HealthRepository _repository
+        private readonly HealthRepository _repository
+            
         public HealthService(HealthRepository healthRepository)
         {
-            _healthRepository healthRepository
+            _repository healthRepository
         }
         public string GetHealthMessage()
         {
-            return "I'm alive!";
+            return _repository.GetHealthMessage();
         }
     }
+}
